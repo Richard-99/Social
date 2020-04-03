3a3b1c70f00667341a3b7774f346271d1297f4fd
Mayank Gupta <g.mayank82@yahoo.com>
2020-04-03 13:38:04 +0530

    Added User models

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+)
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Class to manage user creation"""
+
+    def create_user(self, email, name, password=None):
+        """Creates a new user with the given details"""
+
+        #Checking user has provided an email
+        if not email:
+            return ValueError("Must have an email")
+
+        #Creating a new user object
+        user = self.model(email=self.normalize_email(email), name=name)
+
+        #Hashing the password instead of saving it as plaintext
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Creating a superuser(admin) using the provided details"""
+
+        user = self.create_user(email, name, password)
+
+        #Making the user admin
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """User profile for the social network"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Function to let django know what to user for full name"""
+
+        return self.name
+
+    def __str__(self):
+        """To show on output of an object as string"""
+
+        return self.email
