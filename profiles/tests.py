44d54c9ad9cc29646c9a9be6d3fd76707c8b94c4
Mayank Gupta <g.mayank82@yahoo.com>
2020-04-10 17:58:04 +0530

    Adding tests

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, APIRequestFactory
 
-# Create your tests here.
+from .models import UserProfile, ProfileFeedItem
+from .views import UserProfileFeedViewSet
+
+class UserProfileTest(APITestCase):
+
+    def test_create_profile(self):
+        """Ensuring creating a profile"""
+        data = {
+            "email": "abc@xyz.com",
+            "name": "Test user",
+            "password": "abc123"
+        }
+        response = self.client.post(reverse('api:profile-list'), data,
+                                    format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(UserProfile.objects.count(), 1)
+        self.assertEqual(UserProfile.objects.get().name, 'Test user')
+
+
+class ProfileFeedItemTest(APITestCase):
+
+    def setUp(self):
+
+        user = UserProfile.objects.create_user(email="abc@xyz.com",
+                                                name="Test user",
+                                                password="abc123")
+        self.url = reverse('api:feed-list')
+        self.factory = APIRequestFactory()
+        self.view = UserProfileFeedViewSet.as_view({'get': 'list'})
+        self.user = UserProfile.objects.get(name="Test user")
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+    def test_status_update(self):
+
+        request = self.factory.get(self.url, HTTP_AUTHORIZATION='Token {}'.format(self.token.key))
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- profiles/urls.py
+++ profiles/urls.py
@@ -5,11 +5,11 @@ from rest_framework.routers import DefaultRouter
 from profiles import views
 
 routers = DefaultRouter()
-routers.register('profile', views.UserProfileViewSet)
-routers.register('feed', views.UserProfileFeedViewSet)
-routers.register('message', views.MessagingViewSet)
+routers.register('profile', views.UserProfileViewSet, basename="profile")
+routers.register('feed', views.UserProfileFeedViewSet, basename="feed")
+routers.register('message', views.MessagingViewSet, basename="message")
 
 urlpatterns = [
-    path('', include(routers.urls)),
+    path('', include((routers.urls, 'profiles'), namespace='api')),
     path('login/', views.UserLoginApiView.as_view()),
 ]
